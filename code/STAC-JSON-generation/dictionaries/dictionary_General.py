bd86886ae64ed343cc06682dd857e85b578ab527
Antonio Cozzolino <122694156+cozzolinoac11@users.noreply.github.com>
2023-06-28 16:57:18 +0200

    Create dictionary_General.py

---
+++ code/STAC-JSON-generation/dictionaries/dictionary_General.py
@@ -0,0 +1,15 @@
+# dictionary for general fields (common between ML and non-ML resources)
+def build():
+    dictionary = dict()
+    dictionary['Name of resource'] = 'title'
+    dictionary['ID'] = 'id'
+    dictionary['Description'] = 'description'
+    dictionary['Main category'] = 'main-category'
+    dictionary['Pubblication date'] = 'datetime'
+    dictionary['Keyword'] = 'keywords'
+    dictionary['Platform'] = 'platform'
+    dictionary['Framework'] = 'framework'
+    dictionary['Algorithm'] = 'algorithm'
+    dictionary['Conditions for access and use'] = 'license'
+    dictionary['Biases and ethical aspects'] = 'biases-and-ethical-aspects'
+    return dictionary
