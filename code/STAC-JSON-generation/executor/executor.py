9b0cea2efc22e70f37b3ef88cd8e1d2ec752799e
Antonio Cozzolino <122694156+cozzolinoac11@users.noreply.github.com>
2023-06-28 16:56:05 +0200

    Create executor.py

---
+++ code/STAC-JSON-generation/executor/executor.py
@@ -0,0 +1,86 @@
+from github import Github
+from builder import dictionary_builder
+from builder import builder_interface
+import config
+
+
+# importing values from the configuration file
+# authentication token
+GH_TOKEN = config.GH_TOKEN
+# repository name
+repository = config.repository
+# issue labels to download
+labels = config.labels
+# folders for saving data
+markdown_folder = config.markdown_folder
+stac_files_folder = config.stac_files_folder
+
+
+# execution of the entire application:
+# download of issues -> building of dictionaries -> building of STAC files -> publication on GitHub.
+def run():
+    github_connection = Github(GH_TOKEN)
+    # download all issues with <label = "a/p resource metadata">
+    issues = github_connection.get_repo(repository).get_issues(labels=labels)
+    # iteration for each issue with label='a/p resource metadata'
+    for i in range(issues.totalCount):
+        page = issues.get_page(i)
+        for j in range(len(page)):
+            print('\n---> Executing issue ' + page[j].title + ' <---')
+            title = page[j].title
+            body = page[j].body
+
+            # txt and STAC file generation
+            txt_filename = markdown_folder + title + '.txt'
+            stac_filename = stac_files_folder + title + '.json'
+
+            # writing the txt file
+            with open(txt_filename, 'w+') as f:
+                f.write(body)
+            f.close()
+
+            # dictionary building
+            dictionary = dictionary_builder.get_dictionary(txt_filename)
+            # STAC file building
+            builder_interface.build_stac_file(dictionary, stac_filename)
+
+            # opening the file to be uploaded to GitHub
+            with open(stac_filename, 'r') as stac:
+                stac_file = stac.read()
+            stac.close()
+
+            # with open(txt_filename, 'r') as markdown:
+            #     markdown_file = markdown.read()
+            # markdown.close()
+
+            # opening the GitHub repository
+            repo = github_connection.get_repo(repository)
+
+            # checking if files are in repository -> in this case must be update
+            # markdown_in_repo = False
+            stac_in_repo = False
+            # sha_markdown = ''
+            sha_stac = ''
+            files = repo.get_contents(config.stac_files_repo_folder)
+            for f in range(len(files)):
+                if files[f].path == stac_filename:
+                    stac_in_repo = True
+                    sha_stac = files[f].sha
+                    break
+                # if files[f].path == txt_filename:
+                #     markdown_in_repo = True
+                #     sha_markdown = files[f].sha
+                #     continue
+
+            # publishing to GitHub
+            # if markdown_in_repo:
+            #     repo.update_file(txt_filename, 'update', markdown_file, sha_markdown, branch="master")
+            # else:
+            #     repo.create_file(txt_filename, 'upload', markdown_file, 'master')
+            if stac_in_repo:
+                repo.update_file(stac_filename, 'update', stac_file, sha_stac, branch="main")
+            else:
+                repo.create_file(stac_filename, 'upload', stac_file, branch='main')
+            print('STAC file uploaded...')
+
+    print('Execution completed')
