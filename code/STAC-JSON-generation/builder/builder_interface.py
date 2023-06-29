4430e9aa8b637626ac815e1b0c0321539e1021d0
Antonio Cozzolino <122694156+cozzolinoac11@users.noreply.github.com>
2023-06-29 12:59:22 +0200

    Update builder_interface.py

--- code/STAC-JSON-generation/builder/builder_interface.py
+++ code/STAC-JSON-generation/builder/builder_interface.py
@@ -1,12 +1,19 @@
 import builder.ml_resources
 import builder.no_ml_resources
+import builder.common_blocks
+import config
 
 
 # This function is the bridge between the ML-resource STAC-builder and the no-ML resource STAC-builder.
 # It selects which one to call based on the value of the Main category.
-def build_stac_file(dictionary, path):
+def build_stac_file(dictionary, path, title):
     category = dictionary.get('Main category')
+    title = title+'.json'
     if category == 'Deep Learning' or category == 'Machine Learning':
         builder.ml_resources.build_ml_stac(dictionary, path=path)
+        builder.common_blocks.update_collection(config.stac_files_folder + config.ml_collection + '.json', title)
+        return config.stac_files_folder + config.ml_collection + '.json'
     else:
         builder.no_ml_resources.build_no_ml_stac(dictionary, path=path)
+        builder.common_blocks.update_collection(config.stac_files_folder + config.no_ml_collection + '.json', title)
+        return config.stac_files_folder + config.no_ml_collection + '.json'
